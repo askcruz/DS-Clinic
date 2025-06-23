b5c89446ec2c3d0bd4cdfeaaee5952af1f7c80ed
Felipe-JD <jdtfelipe@mymail.mapua.edu.ph>
2025-06-23 15:07:57 +0800

    Remove unused AboutUs import and add doc-better.svg asset

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import { useNavigate } from 'react-router-dom';
-import AboutUs from '../About Us/AboutUs';
+
 
 const navMap = {
   Home: '/',
